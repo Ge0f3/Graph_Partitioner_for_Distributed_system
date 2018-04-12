520dd0363365aed387fb83d00d97c0ac5099af3b
= <geoffrey.geofe@gmail.com>
2018-04-12 15:59:05 -0500

    initial commit

---
+++ Graph_Partition/src/GraphAlgorithm/Main.java
@@ -0,0 +1,9 @@
+package GraphAlgorithm;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Graph Partition Algorithms \nEnter your choice \n1.AKIN \n2.IOGP");
+    }
+}
