9343b44f5fb6418023f1faf1f650f1a9c91fe175
= <geoffrey.geofe@gmail.com>
2018-04-17 13:04:43 -0500

    change in data structure

--- Graph_Partition/src/GraphAlgorithm/AKIN.java
+++ Graph_Partition/src/GraphAlgorithm/AKIN.java
@@ -8,54 +8,54 @@ import java.util.Map;
 
 
 public class AKIN {
-    LinkedList<Integer> pratition;
-    Map<Integer,Integer> Graph = new HashMap<Integer,Integer>();
-    ArrayList<Integer> partition = new ArrayList<Integer>();
-    AKIN(){
-        System.out.println("Class for AKIN Algorithm");
-    }
-    boolean not_exists(int vertex,ArrayList partition){
-        boolean exist=true;
-
-        return exist;
-    }
-    void DPartition(int vertex,int Partition_size){
-        int i =vertex;
-        if(partition.contains(i)){
-            if(not_exists(vertex,partition)){
-                partition.add(vertex);
-            }
-        }
-
-    }
-    boolean isFull(ArrayList partition){
-        if (){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-    void partition(int src,int destination,ArrayList partition){
-        int i = Graph.get(src);
-        int j = Graph.get(destination);
-        ArrayList t ;
-        if(){
-            int max_score = 0;
-
-            for (Object part:partition) {
-                int x =Graph.get(i);
-                if(x>max_score && isFull(partition)){
-                    max_score =x;
-                    t.add(part);
-                }
-
-            }
-
-        }
-        if(t.isEmpty()){
-
-        }
-
-    }
+//    LinkedList<Integer> pratition;
+//    Map<Integer,Integer> Graph = new HashMap<Integer,Integer>();
+//    ArrayList<Integer> partition = new ArrayList<Integer>();
+//    AKIN(){
+//        System.out.println("Class for AKIN Algorithm");
+//    }
+//    boolean not_exists(int vertex,ArrayList partition){
+//        boolean exist=true;
+//
+//        return exist;
+//    }
+//    void DPartition(int vertex,int Partition_size){
+//        int i =vertex;
+//        if(partition.contains(i)){
+//            if(not_exists(vertex,partition)){
+//                partition.add(vertex);
+//            }
+//        }
+//
+//    }
+//    boolean isFull(ArrayList partition){
+//        if (){
+//            return true;
+//        }
+//        else{
+//            return false;
+//        }
+//    }
+//    void partition(int src,int destination,ArrayList partition){
+//        int i = Graph.get(src);
+//        int j = Graph.get(destination);
+//        ArrayList t ;
+//        if(){
+//            int max_score = 0;
+//
+//            for (Object part:partition) {
+//                int x =Graph.get(i);
+//                if(x>max_score && isFull(partition)){
+//                    max_score =x;
+//                    t.add(part);
+//                }
+//
+//            }
+//
+//        }
+//        if(t.isEmpty()){
+//
+//        }
+//
+//    }
 }
--- Graph_Partition/src/GraphAlgorithm/FileOp.java
+++ Graph_Partition/src/GraphAlgorithm/FileOp.java
@@ -1,17 +1,54 @@
 package GraphAlgorithm;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class FileOp {
     Graph grap = new Graph(349);
+    HashMap<Integer,ArrayList<Integer>> Graph =new HashMap<Integer, ArrayList<Integer>>();
     FileOp(){
-        System.out.println("This is a to do file operation");
+        System.out.println("<------------------------------------------------------------------------   Reading the file ------------------------------------------------ ------------------------>");
+    }
+
+    public boolean isavailable(int vertex){
+        boolean result = Graph.containsKey(vertex);
+        return result;
+//        if(result){
+//            System.out.printf("%s vertex is availabe in the graph",vertex);
+//        }
+//        else{
+//            System.out.printf("%s is not present in the graph !!\n update the graph with the vertex !!\n",vertex);
+//        }
+
+    }
+    public void addEdge(int src,int dest){
+
+        if(isavailable(src)){
+            ArrayList<Integer> temp = new ArrayList<Integer>();
+            temp = Graph.get(src);
+            temp.add(dest);
+            Graph.put(src,temp);
+        }
+        else {
+            ArrayList<Integer> edges =new ArrayList<Integer>();
+            edges.add(dest);
+            Graph.put(src,edges);
+        }
+
+    }
+    public void printGraph(){
+        System.out.println("Printing the vertex with its edge list");
+        for (Integer vertex1:Graph.keySet()) {
+            Integer key = vertex1;
+            ArrayList<Integer> values = Graph.get(key);
+            System.out.printf("%s --> edge list - %s\n",key,values);
+        }
+
     }
     public void ReadFile(){
         Scanner F = null;
-
-
         try{
             F = new Scanner(new File("text_file.txt"));
         }
@@ -19,16 +56,14 @@ public class FileOp {
             System.out.println("Couldn't Find the find the file ");
         }
         while(F.hasNext()){
-            String edge1 = F.next();
-            String edge2 = F.next();
-            int src = Integer.parseInt(edge1);
-            int des = Integer.parseInt(edge2);
-            grap.addEdge(grap,src,des);
+            int edge1 = Integer.parseInt(F.next());
+            int edge2 = Integer.parseInt(F.next());
+            addEdge(edge1,edge2);
             //System.out.printf("%s is connected to %s \n",edge1,edge2);
 
         }
 
         System.out.println("Printing the graph adjacency List");
-        grap.printGraph(grap);
+        printGraph();
     }
 }
